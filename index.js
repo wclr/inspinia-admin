c1dde33b3ffa66c173d4b48ee41ed9ac341e4872
Alex <alex.oschepkov@gmail.com>
2015-08-06 03:55:55 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+// Mainly scripts 
+//require('./static/js/jquery-2.1.1')
+require('./static/js/bootstrap.min')
+require('./static/js/plugins/metisMenu/jquery.metisMenu')
+require('./static/js/plugins/slimscroll/jquery.slimscroll.min')
+
+// Flot 
+require('./static/js/plugins/flot/jquery.flot')
+require('./static/js/plugins/flot/jquery.flot.tooltip.min')
+require('./static/js/plugins/flot/jquery.flot.spline')
+require('./static/js/plugins/flot/jquery.flot.resize')
+require('./static/js/plugins/flot/jquery.flot.pie')
+
+// Peity 
+require('./static/js/plugins/peity/jquery.peity.min')
+require('./static/js/demo/peity-demo')
+
+// Custom and plugin javascript 
+require('./static/js/inspinia')
+require('./static/js/plugins/pace/pace.min')
+
+// jQuery UI 
+require('./static/js/plugins/jquery-ui/jquery-ui.min')
+
+// GITTER 
+require('./static/js/plugins/gritter/jquery.gritter.min')
+
+// Sparkline 
+require('./static/js/plugins/sparkline/jquery.sparkline.min')
+
+// Sparkline demo data  
+require('./static/js/demo/sparkline-demo')
+
+// ChartJS
+require('./static/js/plugins/chartJs/Chart.min')
+
+// Toastr 
+require('./static/js/plugins/toastr/toastr.min')
+
+// Styles
+
+require('./static/css/bootstrap.min.css!')
+require('./static/font-awesome/css/font-awesome.css!')
+
+<!-- Toastr style -->
+require('./static/css/plugins/toastr/toastr.min.css!')
+
+<!-- Gritter -->
+require('./static/js/plugins/gritter/jquery.gritter.css!')
+
+require('./static/css/animate.css!')
+require('./static/css/style.css!')
