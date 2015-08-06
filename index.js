3f335f1dc78738a2510d0af5fa7d1fbcc222e535
Alex <alex.oschepkov@gmail.com>
2015-08-06 13:19:33 +0500

    change base libs imports

--- index.js
+++ index.js
@@ -1,52 +1,51 @@
-// Mainly scripts 
+// jQuery
 //require('./static/js/jquery-2.1.1')
+// jQuery UI
+//require('./static/js/plugins/jquery-ui/jquery-ui.min')
+
 require('./static/js/bootstrap.min')
+require('./static/css/bootstrap.min.css!')
+require('./static/font-awesome/css/font-awesome.css!')
+require('./static/css/animate.css!')
+require('./static/css/style.css!')
+
 require('./static/js/plugins/metisMenu/jquery.metisMenu')
 require('./static/js/plugins/slimscroll/jquery.slimscroll.min')
 
-// Flot 
-require('./static/js/plugins/flot/jquery.flot')
-require('./static/js/plugins/flot/jquery.flot.tooltip.min')
-require('./static/js/plugins/flot/jquery.flot.spline')
-require('./static/js/plugins/flot/jquery.flot.resize')
-require('./static/js/plugins/flot/jquery.flot.pie')
-
-// Peity 
-require('./static/js/plugins/peity/jquery.peity.min')
-require('./static/js/demo/peity-demo')
-
-// Custom and plugin javascript 
+// Custom and plugin javascript
 require('./static/js/inspinia')
 require('./static/js/plugins/pace/pace.min')
 
-// jQuery UI 
-require('./static/js/plugins/jquery-ui/jquery-ui.min')
+// Flot
+//require('./static/js/plugins/flot/jquery.flot')
+//require('./static/js/plugins/flot/jquery.flot.tooltip.min')
+//require('./static/js/plugins/flot/jquery.flot.spline')
+//require('./static/js/plugins/flot/jquery.flot.resize')
+//require('./static/js/plugins/flot/jquery.flot.pie')
 
-// GITTER 
-require('./static/js/plugins/gritter/jquery.gritter.min')
+// Peity 
+//require('./static/js/plugins/peity/jquery.peity.min')
+//require('./static/js/demo/peity-demo')
 
-// Sparkline 
-require('./static/js/plugins/sparkline/jquery.sparkline.min')
 
-// Sparkline demo data  
-require('./static/js/demo/sparkline-demo')
+
+// Gritter
+//require('./static/js/plugins/gritter/jquery.gritter.min')
+//require('./static/js/plugins/gritter/jquery.gritter.css!')
+
+// Sparkline
+//require('./static/js/plugins/sparkline/jquery.sparkline.min')
+
+// Sparkline demo data
+//require('./static/js/demo/sparkline-demo')
 
 // ChartJS
-require('./static/js/plugins/chartJs/Chart.min')
+//require('./static/js/plugins/chartJs/Chart.min')
+//
+//// Toastr - Problems (with js)when building minified
+//require('./static/js/plugins/toastr/toastr.min')
+//require('./static/css/plugins/toastr/toastr.min.css!')
 
-// Toastr 
-require('./static/js/plugins/toastr/toastr.min')
 
-// Styles
 
-require('./static/css/bootstrap.min.css!')
-require('./static/font-awesome/css/font-awesome.css!')
 
-<!-- Toastr style -->
-require('./static/css/plugins/toastr/toastr.min.css!')
-
-<!-- Gritter -->
-require('./static/js/plugins/gritter/jquery.gritter.css!')
-
-require('./static/css/animate.css!')
-require('./static/css/style.css!')
